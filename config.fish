735a6f0360e5f484e7c18c2710e5a6f83a8cdf3c
Evan Chan <evan.chan@socrata.com>
2015-04-20 09:41:53 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -25,16 +25,42 @@ function fish_prompt
   set_color normal
 end
 
+function search-staging
+  knife search node "role:$argv AND chef_environment:azure-staging"
+end
+
+function search-rc
+  knife search node "role:$argv AND chef_environment:azure-rc"
+end
+
+function list-java
+  lsof -a -d cwd -c java
+end
+
 # Environment vars
 # set -x HADOOP_HOME blah blah
+set -x SBT_OPTS "-XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -Xmx8g"
+set -x DOCKER_HOST (bash -c '$(dvm env);echo $DOCKER_HOST')
+set -x EDITOR /usr/bin/vim
+
+set -x DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH /usr/local/lib/rustlib/x86_64-apple-darwin/lib/
+
+# Docker vars
+
+set -x DOCKER_HOST tcp://(boot2docker ip 2>/dev/null):2376
+set -x DOCKER_CERT_PATH /Users/evanchan/.boot2docker/certs/boot2docker-vm
+set -x DOCKER_TLS_VERIFY 1
+
+set PATH ~/bin $PATH
 
 # rbenv support
-set PATH $HOME/.rbenv/bin $PATH
-set PATH $HOME/.rbenv/shims $PATH
-rbenv rehash >/dev/null ^&1
+# set PATH $HOME/.rbenv/bin $PATH
+# set PATH $HOME/.rbenv/shims $PATH
+# rbenv rehash >/dev/null ^&1
+set -gx RBENV_ROOT /usr/local/Cellar/rbenv/0.4.0
+. (rbenv init -|psub)
 
 # Aliases
 alias subl3="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
-alias nimrod="/usr/local/Cellar/nimrod/0.9.2/libexec/bin/nimrod"
 
 # if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
